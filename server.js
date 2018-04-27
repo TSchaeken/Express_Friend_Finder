333e0650cc5c54f9bcb0c3a2491abfa74f46967a
Tim Schaeken <tschaeken@gmail.com>
2018-04-26 18:38:44 -0700

    Updated functionality of get/post

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = require("express");
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
